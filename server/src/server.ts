2873c2aa8f9363d3006e3b3da801ba36c46197dd
Sanira Deneth <sanira.adesha@gmail.com>
2026-04-09 12:58:09 +0530

    chore(server): initialize TypeScript backend with Express

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
