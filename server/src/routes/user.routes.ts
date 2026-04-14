19ca33bf8de09837b9567144297ce5fff04f0bc3
Sanira Deneth <sanira.adesha@gmail.com>
2026-04-15 00:53:15 +0530

    feat: implement complete authorization system and secure routes

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/user.controller.js';
+import { getMe, login, register } from '../controllers/user.controller.js';
 import { asyncHandler } from '../utils/async.handler.js';
 import { validate } from '../middlewares/validate.middleware.js';
 import { userSchema } from '../schemas/user.schema.js';
+import { protect } from '../middlewares/auth.middleware.js';
 
 const userRoutes = Router();
 
@@ -12,5 +13,7 @@ userRoutes.post(
    validate(userSchema.pick({ email: true, password: true })),
    asyncHandler(login)
 );
+userRoutes.use(protect);
+userRoutes.get('/me', asyncHandler(getMe));
 
 export default userRoutes;
--- server/src/utils/async.handler.ts
+++ server/src/utils/async.handler.ts
@@ -1,13 +1,13 @@
 import type { NextFunction, Request, Response } from 'express';
 
-type AsyncHandler = (
-   req: Request,
+type AsyncHandler<T extends Request = Request> = (
+   req: T,
    res: Response,
    next: NextFunction
 ) => Promise<unknown>;
 
-export const asyncHandler = (fn: AsyncHandler) => {
+export const asyncHandler = <T extends Request>(fn: AsyncHandler<T>) => {
    return (req: Request, res: Response, next: NextFunction) => {
-      fn(req, res, next).catch(next);
+      fn(req as T, res, next).catch(next);
    };
 };
