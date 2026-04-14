937e01e56aeba4e28da92a124f94aa1e029f58b4
Sanira Deneth <sanira.adesha@gmail.com>
2026-04-15 03:00:19 +0530

    refactor: standardize cart operations and enhance type safety

--- server/src/routes/cart.routes.ts
+++ server/src/routes/cart.routes.ts
@@ -9,15 +9,28 @@ import {
    clearCart,
 } from '../controllers/cart.controller.js';
 import { asyncHandler } from '../utils/async.handler.js';
+import { protect } from '../middlewares/auth.middleware.js';
+import { validate } from '../middlewares/validate.middleware.js';
+import { addToCartSchema } from '../schemas/cart.schema.js';
 
 const cartRoutes = Router();
 
+cartRoutes.use(protect);
+
 cartRoutes.post('/', asyncHandler(createCart));
-cartRoutes.post('/add', asyncHandler(addToCart));
-cartRoutes.post('/remove', asyncHandler(removeFromCart));
+cartRoutes.post('/add', validate(addToCartSchema), asyncHandler(addToCart));
+cartRoutes.post(
+   '/remove',
+   validate(addToCartSchema),
+   asyncHandler(removeFromCart)
+);
+cartRoutes.post(
+   '/update-quantity',
+   validate(addToCartSchema),
+   asyncHandler(updateCartItemQuantity)
+);
 cartRoutes.post('/remove-item', asyncHandler(removeItemFromCart));
-cartRoutes.post('/update-quantity', asyncHandler(updateCartItemQuantity));
-cartRoutes.post('/clear/:userId', asyncHandler(clearCart));
-cartRoutes.get('/:userId', asyncHandler(getCart));
+cartRoutes.post('/clear', asyncHandler(clearCart));
+cartRoutes.get('/', asyncHandler(getCart));
 
 export default cartRoutes;
